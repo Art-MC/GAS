cb16390d0ae9e18d3c1164ce982861e45cdc777d
Luis Rangel DaCosta <luisrd@berkeley.edu>
2024-05-07 10:14:08 -0700

    add argument to be able to also return a scene copy with just the nanocrystalline seeds

--- gas/generation/nc_seeds.py
+++ gas/generation/nc_seeds.py
@@ -7,19 +7,27 @@ from czone.generator import AmorphousGenerator, Generator
 from czone.molecule import Molecule
 from pymatgen.core import Structure
 
-from utils import get_cell_info, load_and_prepare_xyz, get_nanocrystalline_grains, orient_and_shift_grain
+from utils import (
+    get_cell_info,
+    load_and_prepare_xyz,
+    get_nanocrystalline_grains,
+    orient_and_shift_grain,
+)
 from itertools import repeat
 
 from pathlib import Path
 
+
 def sample_grains(grains, min_dist, N, rng=np.random.default_rng(), max_attempts=32):
     """Sample seed grains with rejection sampling, to ensure minimum spacing"""
-    centers = [np.mean(get_bounding_box(grain.alg_objects)[0], axis=0) for grain in grains]
+    centers = [
+        np.mean(get_bounding_box(grain.alg_objects)[0], axis=0) for grain in grains
+    ]
 
     all_indices = set(np.arange(len(grains)))
     chosen_indices = set([])
     cur_attempt = 0
-    while(len(chosen_indices) < N and cur_attempt < max_attempts):
+    while len(chosen_indices) < N and cur_attempt < max_attempts:
         # Choose a random remaining grain
         next_trial = rng.choice(list(all_indices))
         trial_success = True
@@ -40,20 +48,43 @@ def sample_grains(grains, min_dist, N, rng=np.random.default_rng(), max_attempts
 
     return [grain for i, grain in enumerate(grains) if i in chosen_indices]
 
-def get_nanocrystal_seeds(domain, N_seeds, seed_crystal, min_dist, density, rng=np.random.default_rng(), **kwargs):
+
+def get_nanocrystal_seeds(
+    domain,
+    N_seeds,
+    seed_crystal,
+    min_dist,
+    density,
+    rng=np.random.default_rng(),
+    **kwargs,
+):
     # Get volumes for seed grains
-    all_grains = get_nanocrystalline_grains(min_dist=min_dist, density=density, domain=domain)
+    all_grains = get_nanocrystalline_grains(
+        min_dist=min_dist, density=density, domain=domain
+    )
     seed_grains = sample_grains(all_grains, min_dist=min_dist, N=N_seeds, rng=rng)
 
     # Attach a randomly oriented lattice generator for each seed
     crystal = Structure.from_file(seed_crystal)
     base_crystal_generator = Generator(structure=crystal)
     for grain in seed_grains:
-        grain.add_generator(orient_and_shift_grain(base_crystal_generator.from_generator(), rng, crystal.lattice.a))
+        grain.add_generator(
+            orient_and_shift_grain(
+                base_crystal_generator.from_generator(), rng, crystal.lattice.a
+            )
+        )
 
     return seed_grains
 
-def add_seeds_to_amorphous_block(atoms, pbc, rng=np.random.default_rng(), tolerance=1e-5, **kwargs):
+
+def add_seeds_to_amorphous_block(
+    atoms,
+    pbc,
+    rng=np.random.default_rng(),
+    return_seeds=False,
+    tolerance=1e-5,
+    **kwargs,
+):
     """Starting with a given amorphous block, add in crystalline seeds."""
 
     ## Create a molecular generator with a box domain for the starting block
@@ -72,32 +103,52 @@ def add_seeds_to_amorphous_block(atoms, pbc, rng=np.random.default_rng(), tolera
             p.tol = tolerance
 
     ## Combine into scene and generate atoms
-    scene = PeriodicScene(domain_cell=Voxel(atoms.get_cell()),
-                            objects=[amor_block] + seed_grains,
-                            pbc=pbc,
+    scene = PeriodicScene(
+        domain_cell=Voxel(atoms.get_cell()),
+        objects=[amor_block] + seed_grains,
+        pbc=pbc,
     )
-    
+
     scene.populate()
-    return scene
+
+    if return_seeds:
+        grain_scene = PeriodicScene(
+            domain_cell=scene.domain_cell,
+            objects=seed_grains,
+            pbc=pbc,
+        )
+        grain_scene.populate()
+        return scene, grain_scene
+    else:
+        return scene
 
 
 def main():
-
     ## This will throw an error until in_file is updated
-    in_file = Path('Si_melt.xyz') 
+    in_file = Path("Si_melt.xyz")
     starting_block = load_and_prepare_xyz(in_file)
 
     ## Set up the seed generation
-    crystal_cif = Path('example_files/mp-149_as_Au.cif')
-    config = {'seed_crystal':crystal_cif, # which crystal to use as a seed grain
-              'pbc':(True, True, False), # pbc of scene
-              'min_dist':8, # minimum distance between seed centers, in angstroms
-              'N_seeds':8, # maximum number of seeds to include
-              'density':1e-3, # number density of seed centers, in N * angstrom ^ -3
-              'tolerance':1.0, # amount of excess space to exclude orig liquid atoms from
-            }
-    block_with_seeds = add_seeds_to_amorphous_block(starting_block, **config)
-    block_with_seeds.to_file('SiO_seed_test.xyz')
+    crystal_cif = Path("example_files/mp-149_as_Au.cif")
+    config = {
+        "seed_crystal": crystal_cif,  # which crystal to use as a seed grain
+        "pbc": (True, True, False),  # pbc of scene
+        "min_dist": 8,  # minimum distance between seed centers, in angstroms
+        "N_seeds": 8,  # maximum number of seeds to include
+        "density": 1e-3,  # number density of seed centers, in N * angstrom ^ -3
+        "tolerance": 1.0,  # amount of excess space to exclude orig liquid atoms from
+        "return_seeds": (
+            return_seeds := True
+        ),  # also return a scene with just the seed grains
+    }
+
+    if return_seeds:
+        block_with_seeds, seeds = add_seeds_to_amorphous_block(starting_block, **config)
+        seeds.to_file("SiO_seeds.xyz")
+    else:
+        block_with_seeds = add_seeds_to_amorphous_block(starting_block, **config)
+    block_with_seeds.to_file("SiO_seed_test.xyz")
+
 
 if __name__ == "__main__":
     main()
